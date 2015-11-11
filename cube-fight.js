312bbdc7830830e6f0a846eb3b61f204f17cbbdd
Donnie Coleman <donnie.coleman@gmail.com>
2015-11-10 21:48:19 -0500

    added ability to update new player's cube to the current state of the cube

--- cube-fight.js
+++ cube-fight.js
@@ -17,7 +17,11 @@ function getMove(index){
 function getMoves(){
   return Moves.find({}, {sort:{createdAt: -1}}).fetch();
 }
+function parseMove(move, left){
+  move = move.split('');
+  return {face: move[0], slice: move[1], rotate: (left ? 'left' : 'right')};
 
+}
 if (Meteor.isClient) {
   Template.moves.helpers({
     move: function () {
@@ -46,31 +50,49 @@ if (Meteor.isClient) {
   });
  
   Moves.find().observeChanges({
-    added: function(){      
+    added: function(){
+      if(!window.cube || !window.cube._cube) return;
       var moves = getMoves();
       var move = getMove(moves[0].move);
       //console.log("Moves: "+moves.map(function(move){ return "["+getMove(move.move)+":"+move.createdAt.toLocaleTimeString()+"]";}));
       //console.log("Move: "+move);
       if(move && move!== 'on'){
-          move = move.split('');
           window.cube._expectingTransition = true;
-          window.cube._doMovement({face: move[0], slice: move[1], rotate: 'right'});
+          window.cube._doMovement(parseMove(move));
       }
     },
     removed: function(){
-        if(Moves.find().count() === 0) {
-          window.cube._solveFake();
-        }
+      if(!window.cube || !window.cube._cube) return;
+      if(Moves.find().count() === 0) {
+        window.cube._solveFake();
+      }
     }
   });
 
   Meteor.startup(function () {
     $.getScript('http://yui.yahooapis.com/3.5.1/build/yui/yui-min.js', function(){
       initCube();
-      YUI().use('node','rubik',function(Y){
-          var cube = window.cube = new Y.Rubik();
+
+      YUI().use('node','rubik', 'rubik-queue', function(Y){
+          var queue = null;
+          if(Moves.find().count() > 0) {
+              queue = new Y.Queue();
+              for(move of getMoves()){
+                queue.add(parseMove(getMove(move.move), true));
+              }
+              queue.current = -1;
+              queue._queue.reverse();
+          }
+
+          var cube = window.cube = (queue ? new Y.Rubik({queue:queue}) : new Y.Rubik());
           // cube._disabledFLick = true;
-          cube.run();    
+          cube.run();
+
+          var intervalId = setInterval(function(){
+            cube._expectingTransition = true;
+            if(!cube._redoMove())
+              clearInterval(intervalId);
+          }, 500);
       });
     });
   });
